e4d78fa5581cf855639c3de5ef8dfd343eeea0e1
Saklani Pankaj <31514745+saklanipankaj@users.noreply.github.com>
2017-09-07 19:47:53 +0800

    Add files via upload

---
+++ PlaneSeat.java
@@ -0,0 +1,33 @@
+
+public class PlaneSeat {
+	private int seatId;
+	private boolean assigned;
+	private int customerId;
+	
+	public PlaneSeat(int seat_Id){
+		seatId = seat_Id;
+	}
+	
+	public int getSeatID(){
+		return seatId;
+	}
+	
+	public int getCustomerID()
+	{
+		return customerId;
+	}
+	
+	public boolean isOccupied(){
+		return assigned;
+	}
+	
+	public void assign(int cust_id){
+		assigned = true;
+		customerId = cust_id;
+	}
+	
+	public void unAssign(){
+		assigned = false;
+		customerId = 0;
+	}
+}
